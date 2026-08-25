15f7b253964bc5f12f0df1e6dbb0085923adbc0e
赵志伟 <44900340+sdwurg180507280211@users.noreply.github.com>
2026-08-25 14:22:10 +0800

    feat(desktop): expose safe local service access

--- electron-preload.js
+++ electron-preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('desktopBridge', {
-  openMainWindow: () => ipcRenderer.send('desktop:open-main')
+  openMainWindow: () => ipcRenderer.send('desktop:open-main'),
+  openExternal: (url) => ipcRenderer.invoke('desktop:open-external', url)
 });
