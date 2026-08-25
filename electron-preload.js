db5209864099096bdcd4c065c9beb79a620ff639
赵志伟 <44900340+sdwurg180507280211@users.noreply.github.com>
2026-08-25 10:51:33 +0800

    feat(desktop): expose safe desktop window bridge

---
+++ electron-preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('desktopBridge', {
+  openMainWindow: () => ipcRenderer.send('desktop:open-main'),
+  hideWindow: () => ipcRenderer.send('desktop:hide'),
+  setAlwaysOnTop: (value) => ipcRenderer.invoke('desktop:set-always-on-top', Boolean(value))
+});
