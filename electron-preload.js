b7f44cf98ffa8a5e611640d298a1b2d11f63e7d6
赵志伟 <44900340+sdwurg180507280211@users.noreply.github.com>
2026-08-25 12:16:25 +0800

    refactor(desktop): remove obsolete directory picker bridge

--- electron-preload.js
+++ electron-preload.js
@@ -3,6 +3,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('desktopBridge', {
   openMainWindow: () => ipcRenderer.send('desktop:open-main'),
   hideWindow: () => ipcRenderer.send('desktop:hide'),
-  setAlwaysOnTop: (value) => ipcRenderer.invoke('desktop:set-always-on-top', Boolean(value)),
-  selectDirectory: () => ipcRenderer.invoke('desktop:select-directory')
+  setAlwaysOnTop: (value) => ipcRenderer.invoke('desktop:set-always-on-top', Boolean(value))
 });
