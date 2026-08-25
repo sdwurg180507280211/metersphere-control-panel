13468802f197b0a32841f7cd9e2c34038033c310
赵志伟 <44900340+sdwurg180507280211@users.noreply.github.com>
2026-08-25 11:00:04 +0800

    feat(desktop): expose native project directory picker

--- electron-preload.js
+++ electron-preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('desktopBridge', {
   openMainWindow: () => ipcRenderer.send('desktop:open-main'),
   hideWindow: () => ipcRenderer.send('desktop:hide'),
-  setAlwaysOnTop: (value) => ipcRenderer.invoke('desktop:set-always-on-top', Boolean(value))
+  setAlwaysOnTop: (value) => ipcRenderer.invoke('desktop:set-always-on-top', Boolean(value)),
+  selectDirectory: () => ipcRenderer.invoke('desktop:select-directory')
 });
