fb24818fd159985476bf68c4f411d8b6df195304
赵志伟 <978964140@qq.com>
2026-09-05 13:02:49 +0800

    test(desktop): exclude packaged copies from Jest discovery

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   testMatch: ['**/__tests__/**/*.test.js'],
   collectCoverageFrom: ['backend/services/**/*.js'],
   coveragePathIgnorePatterns: ['/node_modules/']
